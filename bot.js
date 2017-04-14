e58f74d75720016db2d1f6f775aef0461924e3e0
Breogán Hackett <breogan.hackett2@mail.dcu.ie>
2017-04-14 04:21:45 +0100

    It tweets!

--- bot.js
+++ bot.js
@@ -2,5 +2,15 @@ console.log("Bot running");
 
 
 let Twit = require('twit');
-let config = require('./config');
+let config = require('./secrets');
 let T = new Twit(config);
+
+let tweet = {
+  status: 'I am an entity that has the ability to transport visual information between universes.'
+};
+
+T.post('statuses/update', tweet, tweeted);
+
+function tweeted(err, data, response) {
+  console.log("tweeted: " + data.text);
+};
